e4d429fa5d7dd87f24c6bdfb0c011f691eb915c5
DaveMullally <davemullally@me.com>
2017-12-13 14:37:57 -0600

    Add files via upload

---
+++ examples/tools/testit_no_runGau.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -x
+
+cd ../tools
+rm outfile
+cd test
+# set up the Gaussian environment
+
+echo "test-Intentional problems" >> ../outfile 2>&1
+../CheckGauInput test.com >> ../outfile
+../CheckGauInput test.com Gaussian_not_in_path >> ../outfile
+cd ..
+
+diff -b -B outfile OUTPUT/out_no_runGau
+
+exit
---
+++ examples/tools/testit_runGau.sh
@@ -0,0 +1,56 @@
+#! /bin/bash -x
+
+cd ../tools
+rm outfile
+cd test
+# set up the Gaussian environment
+
+echo "test-Intentional problems" >> ../outfile 2>&1
+../CheckGauInput test.com >> ../outfile
+../CheckGauInput test.com Gaussian_not_in_path >> ../outfile
+
+export GauBINARY="${GAU_BINARY}"
+
+echo "test-Should be no problems">> ../outfile
+../CheckGauInput test.com ${GauBINARY} >> ../outfile
+cd ..
+
+cd test1071
+echo "test1071-Should be no problems" >> ../outfile
+../CheckGauInput test1071.com ${GauBINARY} >> ../outfile
+cd ..
+
+cd test1129
+echo "test1129-Should be no problems" >> ../outfile
+../CheckGauInput test1129.com ${GauBINARY} >> ../outfile
+cd ..
+
+cd test1130
+echo "test1130-Should be no problems" >> ../outfile
+../CheckGauInput test1130.com ${GauBINARY} >> ../outfile
+cd ..
+
+cd test1132
+echo "test1132-Run Input should be no problems" >> ../outfile
+../CheckGauInput test1132.com ${GauBINARY} >> ../outfile
+echo "test1132-MatrixFile" >> ../outfile
+../CheckGauInput test1132r.dat ${GauBINARY} >> ../outfile
+echo "test1132-Run Input and MatrixFile" >> ../outfile
+../CheckGauInput test1132.com test1132gr.dat test1132r.dat test1132u.dat ${GauBINARY} >> ../outfile
+cd ..
+
+ diff -b -B outfile OUTPUT/out_runGau
+
+exit
+# tests 1135 and 1136 require R.  So a pain to run.
+cd test1135
+echo "test1135" >> ../outfile
+../CheckGauInput test1135.com g16 >> ../outfile
+cd ..
+
+cd test1136
+echo "test1136" >> ../outfile
+../CheckGauInput test1136.com g16 >> ../outfile
+cd ..
+
+
